a2167ec9367e999841260f40bd75daf7ca986c3f
patrico654 <patrico654@users.noreply.github.com>
2018-05-02 12:15:25 +0200

    min of 3

---
+++ lib/min_of_three.rb
@@ -0,0 +1,14 @@
+def min_of_three(number1, number2, number3)
+    i=0
+    arraty = [number1, number2, number3]
+    while arraty(i) < 
+
+        
+    end
+
+
+
+
+
+end 
+puts min_of_three(1, 2 ,3)
